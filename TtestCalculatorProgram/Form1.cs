8268ae319b1763b9c59b2fcd989bc4a9648d5f09
James Hunt <jameshuntwork1997@yahoo.com>
2017-10-24 15:42:05 +0100

    Stage 2
    In this version of the program I have added nearly all functionality. User is now able to calculate all values needed up to the point of retrieving the actual t value. In the next iteration of the code, this feature will be added and code project will be complete!

--- TtestCalculatorProgram/Form1.cs
+++ TtestCalculatorProgram/Form1.cs
@@ -58,37 +58,59 @@ namespace TtestCalculatorProgram
             }
         }
 
-        public int GetAverage(List<int> list) // Method used to get the average from an array list
+        private double GetAverage(List<double> list) // Method used to get the average from an array list
         {
             int len = list.Count;
+            double sum = 0;
 
             for (int i = 0; i < len; i++)
             {
-
+                sum += list[i];
             }
 
-            return 1; // Return average
+            return sum / len; // Return average
+        }
+
+        private List<double> SubAVGThenSquare(List<double> list, double average) // Method used to go through each column, subtract AVG from current value and then square that value
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                list[i] = (list[i] - average) * (list[i] - average);
+            }
+            return list;
+        }
+
+        private double GetS2Numbers(List<double> list)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                sum += list[i];
+            }
+
+            return sum / 9; // Return S2 Value
         }
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
 
-            List<int> dataSetOne = new List<int>(); // Array list to store inputs from column 1
-            List<int> dataSetTwo = new List<int>(); // Array list to store inputs from column 2
+            List<double> dataSetOne = new List<double>(); // Array list to store inputs from column 1
+            List<double> dataSetTwo = new List<double>(); // Array list to store inputs from column 2
 
             try
             {
                 // Gather the inputs from column 1:
                 for (int i = 0; i < 10; i++)
                 {
-                    int val = Convert.ToInt32(this.dataGridView1[0, i].Value);
+                    double val = Convert.ToDouble(this.dataGridView1[0, i].Value);
                     dataSetOne.Add(val);
                 }
 
                 // Gather the inputs from column 2:
                 for (int i = 0; i < 10; i++)
                 {
-                    int val = Convert.ToInt32(this.dataGridView1[1, i].Value);
+                    double val = Convert.ToDouble(this.dataGridView1[1, i].Value);
                     dataSetTwo.Add(val);
                 }
 
@@ -97,6 +119,20 @@ namespace TtestCalculatorProgram
             {
                 MessageBox.Show(ex.Message.ToString());
             }
+
+            // Get average from both array lists:
+            double averageX = GetAverage(dataSetOne);
+            double averageY = GetAverage(dataSetTwo);
+
+            // Get new lists:
+            dataSetOne = SubAVGThenSquare(dataSetOne, averageX);
+            dataSetTwo = SubAVGThenSquare(dataSetTwo, averageY);
+
+            // Get S2 Values:
+            double S2ValueX = GetS2Numbers(dataSetOne);
+            double S2ValueY = GetS2Numbers(dataSetTwo);
+
+
         }
     }
 }
