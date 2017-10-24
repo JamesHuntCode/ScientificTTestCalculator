c6fcd7c2568d1f565385749c5f1d957b9c6ad8a7
James Hunt <jameshuntwork1997@yahoo.com>
2017-10-24 15:10:38 +0100

    Stage 1
    In this stage I have allowed the user to input 2 sets of data (both 10 in row length) in order to calculate their t value.

---
+++ TtestCalculatorProgram/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace TtestCalculatorProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtDisplayResult = new System.Windows.Forms.Label();
+            this.txtResultHeader = new System.Windows.Forms.Label();
+            this.Col1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Col2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "t Test Calculator";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Col1,
+            this.Col2});
+            this.dataGridView1.Location = new System.Drawing.Point(10, 62);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(342, 373);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(10, 456);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(118, 42);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate!";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtDisplayResult
+            // 
+            this.txtDisplayResult.AutoSize = true;
+            this.txtDisplayResult.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDisplayResult.Location = new System.Drawing.Point(87, 519);
+            this.txtDisplayResult.Name = "txtDisplayResult";
+            this.txtDisplayResult.Size = new System.Drawing.Size(52, 24);
+            this.txtDisplayResult.TabIndex = 6;
+            this.txtDisplayResult.Text = "0.00";
+            // 
+            // txtResultHeader
+            // 
+            this.txtResultHeader.AutoSize = true;
+            this.txtResultHeader.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultHeader.Location = new System.Drawing.Point(6, 519);
+            this.txtResultHeader.Name = "txtResultHeader";
+            this.txtResultHeader.Size = new System.Drawing.Size(75, 24);
+            this.txtResultHeader.TabIndex = 7;
+            this.txtResultHeader.Text = "Result:";
+            // 
+            // Col1
+            // 
+            this.Col1.Frozen = true;
+            this.Col1.HeaderText = "DATA SET 1:";
+            this.Col1.Name = "Col1";
+            this.Col1.Width = 150;
+            // 
+            // Col2
+            // 
+            this.Col2.Frozen = true;
+            this.Col2.HeaderText = "DATA SET 2:";
+            this.Col2.Name = "Col2";
+            this.Col2.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(364, 552);
+            this.Controls.Add(this.txtResultHeader);
+            this.Controls.Add(this.txtDisplayResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Scientific t -Test Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label txtDisplayResult;
+        private System.Windows.Forms.Label txtResultHeader;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Col1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Col2;
+    }
+}
+
---
+++ TtestCalculatorProgram/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TtestCalculatorProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.dataGridView1.RowTemplate.Height = 35;
+        }
+
+        private void Form1_Load(object sender, EventArgs e) 
+        {
+            // Steps:
+
+            // Step 1:
+
+                // Set up two columnbs with var 10 rows each
+
+                // Take average of column 1 (all 10 rows) and then field 2 (all 10 rows)
+
+                // Go through the column and subtract the average from each input of that row and then square it
+
+                // Do this for both columns
+
+            // Step 2:
+
+                // Calculate S squared values
+
+                // Add all NEW row data together & then divide it by number of inputs minus 1
+
+                // This then gives us two different values of S squared (from our 2 different columns) 
+
+            // Step 3:
+
+                // Calculate t value (1 value) 
+
+                // Take averages the original AVG values from column 1 & 2
+
+                // Subtract OG AVG value from column 1 from OG AVG value of column 2
+
+                // Divide that number by the square root of the s2 value of col1 + the s2 value of col2 over the number of rows in each column
+
+            // Set up 10 rows:
+
+            for (int i = 0; i < 10; i++)
+            {
+                this.dataGridView1.Rows.Add();
+            }
+        }
+
+        public int GetAverage(List<int> list) // Method used to get the average from an array list
+        {
+            int len = list.Count;
+
+            for (int i = 0; i < len; i++)
+            {
+
+            }
+
+            return 1; // Return average
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+
+            List<int> dataSetOne = new List<int>(); // Array list to store inputs from column 1
+            List<int> dataSetTwo = new List<int>(); // Array list to store inputs from column 2
+
+            try
+            {
+                // Gather the inputs from column 1:
+                for (int i = 0; i < 10; i++)
+                {
+                    int val = Convert.ToInt32(this.dataGridView1[0, i].Value);
+                    dataSetOne.Add(val);
+                }
+
+                // Gather the inputs from column 2:
+                for (int i = 0; i < 10; i++)
+                {
+                    int val = Convert.ToInt32(this.dataGridView1[1, i].Value);
+                    dataSetTwo.Add(val);
+                }
+
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
